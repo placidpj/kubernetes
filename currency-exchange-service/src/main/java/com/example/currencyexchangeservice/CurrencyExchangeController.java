d0c959cc08d23fe849898d2d81da89d53c022e1b
placidpj <106440023+placidpj@users.noreply.github.com>
2023-01-26 23:56:26 +0530

    Added custom environment variable

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -33,7 +33,7 @@ public class CurrencyExchangeController {
 		 
 		 // CHANGE-JUBERNETES
 		 String host = environment.getProperty("HOSTNAME");
-		 String version = "v11";
+		 String version = "v12";
 		 
 		 currencyExchange.setEnvironment(port + " " + version + " " + host);
 		 return currencyExchange;
